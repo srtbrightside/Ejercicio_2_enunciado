7cce51504bccc15d3a48e40c5a789a2d10c690af
Natalia <n.v@outlook.cl>
2017-05-07 21:33:49 -0300

    se agregaron cuadros de comentarios a todas las miembros

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,19 +19,19 @@ var impre = document.getElementById('lista');
 var res1 = "<b><li>Nombre y Apellido: </b>" + espe.nombre + "<br></li><b><li>Edad: </b>" + espe.edad + "<br></li><b><li>Hobbie: </b>" + espe.hobbie
     + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
     var res2 = "<b><li>Nombre y Apellido: </b>" + cindy.nombre + "<br></li><b><li>Edad: </b>" + cindy.edad + "<br></li><b><li>Hobbie: </b>" + cindy.hobbie
-    + "<br><br>";
+    + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
 var res3 = "<b><li>Nombre y Apellido: </b>" + vale.nombre + "<br></li><b><li>Edad: </b>" + vale.edad + "<br></li><b><li>Hobbie: </b>" + vale.hobbie
-    + "<br><br>";
+    + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
 var res4 = "<b><li>Nombre y Apellido: </b>" + fer.nombre + "<br></li><b><li>Edad: </b>" + fer.edad + "<br></li><b><li>Hobbie: </b>" + fer.hobbie
-    + "<br><br>";
+    + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
 var res5 = "<b><li>Nombre y Apellido: </b>" + diana.nombre + "<br></li><b><li>Edad: </b>" + diana.edad + "<br></li><b><li>Hobbie: </b>" + diana.hobbie
-    + "<br><br>";
+    + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
 var res6 = "<b><li>Nombre y Apellido: </b>" + vanessa.nombre + "<br></li><b><li>Edad: </b>" + vanessa.edad + "<br></li><b><li>Hobbie: </b>" + vanessa.hobbie
-    + "<br><br>";
+    + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
 var res7 = "<b><li>Nombre y Apellido: </b>" + natalia.nombre + "<br></li><b><li>Edad: </b>" + natalia.edad + "<br></li><b><li>Hobbie: </b>" + natalia.hobbie
-    + "<br><br>";
+    + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
 var res8 = "<b><li>Nombre y Apellido: </b>" + vane.nombre + "<br></li><b><li>Edad: </b>" + vane.edad + "<br></li><b><li>Hobbie: </b>" + vane.hobbie
-    + "<br><br>";
+    + "<br><br>" + "<textarea name='textarea' rows='9' cols='60'>Ingresa tu comentario</textarea>";
 
 var res = res1 + res2 + res3 + res4 + res5 + res6 + res7 + res8;
 
